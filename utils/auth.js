18a403ed78ec11b57597f369a7f5c92185984e66
James Lynch <jimlynch268@gmail.com>
2023-08-16 12:37:53 -0400

    add pagination

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,4 @@
 const auth = (req, res, next) => {
-  console.log(req.session);
   if (!req.session.userToken) {
     res.json({ message: 'You are not authorized. Please login' });
   // eslint-disable-next-line no-useless-return
