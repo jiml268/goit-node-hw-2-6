2fdeec125e2fcc6dafae5df477268648d3a24e1e
James Lynch <jimlynch268@gmail.com>
2023-08-17 14:53:58 -0400

    set up avatra route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const auth = require('../utils/auth'); 
 const signedIn = require('../utils/signedIn');
 const { getContacts, createContact, getSingleContacts, deleteContacts, updateContacts, updateStatusContact } = require('../controllers')
-const {userLogin, createUser, userLogout, userCurrent,patchUser } = require('../controllers')
+const {userLogin, createUser, userLogout, userCurrent,patchUser, uploadFile} = require('../controllers')
 router.route('/contacts').get(auth, getContacts).post(auth, createContact)
 router.route('/contacts/:id').get(auth, getSingleContacts).delete(auth, deleteContacts).put(auth, updateContacts)
 router.route('/contacts/:id/favorite').patch(auth, updateStatusContact)
@@ -13,8 +13,8 @@ router.route('/users/signup').post(signedIn, createUser)
 router.route('/users/login').post(signedIn, userLogin)
 router.route('/users/logout').post(userLogout)
 router.route('/users/current').get(userCurrent)
-router.route('/patch/users').patch(patchUser)
-
+router.route('/users/patch').patch(patchUser)
+router.route('/users/avatar').post(uploadFile)
 
 
 
